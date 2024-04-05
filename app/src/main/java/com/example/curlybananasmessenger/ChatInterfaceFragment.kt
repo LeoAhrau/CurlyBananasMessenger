f7b60d5eb658e565135098a09279e9cd5a41ca6b
jhaard <jorgen.hard@gmail.com>
2024-04-05 08:25:30 +0200

    Added simple list with test-code in list-view.

--- app/src/main/java/com/example/curlybananasmessenger/ChatInterfaceFragment.kt
+++ app/src/main/java/com/example/curlybananasmessenger/ChatInterfaceFragment.kt
@@ -1,18 +1,56 @@
 package com.example.curlybananasmessenger
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import com.example.curlybananasmessenger.databinding.FragmentChatInterfaceBinding
 
 class ChatInterfaceFragment : Fragment() {
+    private lateinit var binding: FragmentChatInterfaceBinding
+    private lateinit var messageField: EditText
+    private lateinit var sendButton: Button
+    private lateinit var chatList: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+    private val itemList = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_interface, container, false)
+
+        binding = FragmentChatInterfaceBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Testing
+        messageField = binding.etChatMessage
+        sendButton = binding.btnSendMessage
+        chatList = binding.lvChat
+
+        adapter = ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1, itemList)
+        chatList.adapter = adapter
+
+        sendButton.setOnClickListener {
+            val chatMessage = messageField.text.toString()
+            if (chatMessage.isNotEmpty()) {
+                itemList.add(chatMessage)
+                adapter.notifyDataSetChanged() // Change this.
+                messageField.text.clear()
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/example/curlybananasmessenger/MainActivity.kt
+++ app/src/main/java/com/example/curlybananasmessenger/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.curlybananasmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.curlybananasmessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Testing
+        val fragment = supportFragmentManager.beginTransaction()
+
+        fragment.replace(binding.frameContainerTest.id, ChatInterfaceFragment())
+        fragment.addToBackStack(null)
+        fragment.commit()
 
 
     }
