383ab4f04e645b622758fe438bb7ed72a3b10d63
Elinn <aelin6277@gmail.com>
2024-04-12 13:05:16 +0200

    Room implemented in UI and in databaseInspector

--- app/src/main/java/com/example/curlybananasmessenger/MainActivity.kt
+++ app/src/main/java/com/example/curlybananasmessenger/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.curlybananasmessenger
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.curlybananasmessenger.databinding.ActivityMainBinding
@@ -12,7 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_chat)
 
+        // Starta ChatActivity direkt
+        val intent = Intent(this, ChatActivity::class.java)
+        startActivity(intent)
+
+        // Avsluta MainActivity så att den inte är aktiv i backstack
+        finish()
 
         // ------------------- Testing---------------------
         val fragment = supportFragmentManager.beginTransaction()
--- app/src/main/java/com/example/curlybananasmessenger/User.kt
+++ app/src/main/java/com/example/curlybananasmessenger/User.kt
@@ -1,18 +1,6 @@
 package com.example.curlybananasmessenger
 
 
-/*import androidx.room.Entity
-import androidx.room.PrimaryKey
-//definiera en entitet för användare i din Room-databas.
-
-@Entity(tableName = "users")
-data class User(
-    @PrimaryKey val userId: String,
-    val name: String,
-    val email: String
-)
-*/
-
 data class User(val id: String?, val nickname: String?, val username: String?, val password: String?)
 
 
