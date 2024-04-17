94779a72a69ba779c22ced27bcf276155b3b5ec9
jhaard <jorgen.hard@gmail.com>
2024-04-17 14:39:57 +0200

    Drawer working but missing details.

--- app/src/main/java/com/example/curlybananasmessenger/BaseActivity.kt
+++ app/src/main/java/com/example/curlybananasmessenger/BaseActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
@@ -18,20 +20,43 @@ import com.google.android.material.navigation.NavigationView
 
     open lateinit var drawerLayout: DrawerLayout
     open lateinit var  actionBarDrawerToggle: ActionBarDrawerToggle
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
-        // init drawer layout
-        drawerLayout = findViewById(R.id.drawer_layout)
-        // init action bar drawer toggle
-        actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
-        // add a drawer listener into  drawer layout
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
 
-        // show menu icon and back icon while drawer open
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
+     override fun setContentView(view: View?) {
+
+         drawerLayout = layoutInflater.inflate(R.layout.activity_base, null) as DrawerLayout
+         val container = drawerLayout.findViewById<FrameLayout>(R.id.frame_container) // OBS! Container
+         container.addView(view)
+         super.setContentView(drawerLayout)
+
+         val toolbar = drawerLayout.findViewById<Toolbar>(R.id.toolbar)
+         setSupportActionBar(toolbar)
+         val navigationView = drawerLayout.findViewById<NavigationView>(R.id.nav_view)
+         navigationView.setNavigationItemSelectedListener {
+             onOptionsItemSelected(it)
+         }
+
+         val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+         drawerLayout.addDrawerListener(toggle)
+         toggle.syncState()
+
+
+
+
+     }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_base)
+//        // init drawer layout
+//        drawerLayout = findViewById(R.id.drawer_layout)
+//        // init action bar drawer toggle
+//        actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+//        // add a drawer listener into  drawer layout
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+//        actionBarDrawerToggle.syncState()
+//
+//        // show menu icon and back icon while drawer open
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // check conndition for drawer item with menu item
@@ -43,3 +68,5 @@ import com.google.android.material.navigation.NavigationView
 
     }
 }
+
+
