0dcc5bd757362be761b3d3caec2d76595f8d486f
jhaard <jorgen.hard@gmail.com>
2024-04-24 00:21:07 +0200

    Updates messages when sending, but listener does not work when receiving. Order still not working.

--- app/src/main/java/com/example/curlybananasmessenger/ChatInterfaceFragment.kt
+++ app/src/main/java/com/example/curlybananasmessenger/ChatInterfaceFragment.kt
@@ -12,8 +12,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.curlybananasmessenger.databinding.FragmentChatInterfaceBinding
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class ChatInterfaceFragment : Fragment() {
     private lateinit var binding: FragmentChatInterfaceBinding
@@ -112,43 +115,44 @@ class ChatInterfaceFragment : Fragment() {
     }
 
 
-        fun getAllMessages() {
-            val chatList = ArrayList<ChatMessage>()
-            val messagesCollectionSenderPath = "users/$currentUserUid/contacts/$currentContactId/messages"
-            val messagesCollectionReceiverPath = "users/$currentContactId/contacts/$currentUserUid/messages"
+    fun getAllMessages() {
+        val chatList = ArrayList<ChatMessage>()
+        val messagesCollectionSenderPath =
+            "users/$currentUserUid/contacts/$currentContactId/messages"
+        val messagesCollectionReceiverPath =
+            "users/$currentContactId/contacts/$currentUserUid/messages"
 
-            FirebaseFirestore
-                .getInstance()
-                .collection(messagesCollectionSenderPath)
-                .get()
-                .addOnSuccessListener { result ->
-                    for (document in result) {
-                        val text = document.getString("message")
-                        val sender = document.getString("senderId")
-                        val chatMessage = ChatMessage(text, sender)
-                        chatList.add(chatMessage)
-                    }
+        FirebaseFirestore
+            .getInstance()
+            .collection(messagesCollectionSenderPath)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val text = document.getString("message")
+                    val sender = document.getString("senderId")
+                    val chatMessage = ChatMessage(text, sender)
+                    chatList.add(chatMessage)
+                }
 
-                    Log.i("SUCCESS", "Successfully fetched all messages")
-                    showMessages(chatList)
-                }.addOnFailureListener { Log.e("ERROR", "Error fetching messages") }
+                Log.i("SUCCESS", "Successfully fetched all messages")
+                showMessages(chatList)
+            }.addOnFailureListener { Log.e("ERROR", "Error fetching messages") }
 
-            FirebaseFirestore
-                .getInstance()
-                .collection(messagesCollectionReceiverPath)
-                .get()
-                .addOnSuccessListener { result ->
-                    for (document in result) {
-                        val text = document.getString("message")
-                        val sender = document.getString("senderId")
-                        val chatMessage = ChatMessage(text, sender)
-                        chatList.add(chatMessage)
-                    }
+        FirebaseFirestore
+            .getInstance()
+            .collection(messagesCollectionReceiverPath)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val text = document.getString("message")
+                    val sender = document.getString("senderId")
+                    val chatMessage = ChatMessage(text, sender)
+                    chatList.add(chatMessage)
+                }
 
-                    Log.i("SUCCESS", "Successfully fetched all messages")
-                    showMessages(chatList)
-                }.addOnFailureListener { Log.e("ERROR", "Error fetching messages") }
-
+                Log.i("SUCCESS", "Successfully fetched all messages")
+                showMessages(chatList)
+            }.addOnFailureListener { Log.e("ERROR", "Error fetching messages") }
 
 
     }
@@ -161,11 +165,14 @@ class ChatInterfaceFragment : Fragment() {
 
     }
 
-    fun listenToChanges(){
-        val messagesCollectionSenderPath = "users/$currentUserUid/contacts/$currentContactId/messages"
-        val messagesCollectionReceiverPath = "users/$currentContactId/contacts/$currentUserUid/messages"
-        FirebaseFirestore
-            .getInstance()
+    fun listenToChanges() {
+        val instance = FirebaseFirestore.getInstance()
+        val messagesCollectionSenderPath =
+            "users/$currentUserUid/contacts/$currentContactId/messages"
+        val messagesCollectionReceiverPath =
+            "users/$currentContactId/contacts/$currentUserUid/messages"
+        val combinedChatMessages = ArrayList<ChatMessage>()
+        instance
             .collection(messagesCollectionSenderPath)
             .addSnapshotListener(requireActivity()) { value, error ->
                 if (error != null) {
@@ -174,36 +181,84 @@ class ChatInterfaceFragment : Fragment() {
                 if (value != null) {
                     val chatList = ArrayList<ChatMessage>()
                     for (document in value) {
-                        val text = document.getString("message")
-                        val sender = document.getString("senderId")
-                        val chatMessage = ChatMessage(text, sender)
-                        chatList.add(chatMessage)
+                        val chatMessage = document.toObject(ChatMessage::class.java)
+                        chatMessage?.let {
+                            combinedChatMessages.add(it)
+                        }
                     }
                     Log.i("SUCCESS", "Snapshot success")
-                    showMessages(chatList)
                 }
             }
-//
-//        FirebaseFirestore
-//            .getInstance()
-//            .collection(messagesCollectionReceiverPath)
-//            .addSnapshotListener(requireActivity()) { value, error ->
-//                if (error != null) {
-//                    Log.e("ERROR", "failed to listen for updates")
-//                }
-//                if (value != null) {
-//                    val chatList = ArrayList<ChatMessage>()
-//                    for (document in value) {
-//                        val text = document.getString("message")
-//                        val sender = document.getString("senderId")
-//                        val chatMessage = ChatMessage(text, sender)
-//                        chatList.add(chatMessage)
-//                    }
-//                    Log.i("SUCCESS", "Snapshot success")
-//                    showMessages(chatList)
-//                }
-//            }
+
+        instance
+            .collection(messagesCollectionReceiverPath)
+            .addSnapshotListener(requireActivity()) { value, error ->
+                if (error != null) {
+                    Log.e("ERROR", "failed to listen for updates")
+                }
+                if (value != null) {
+                    val chatList = ArrayList<ChatMessage>()
+                    for (document in value) {
+                        val chatMessage = document.toObject(ChatMessage::class.java)
+                        chatMessage?.let {
+                            combinedChatMessages.add(it)
+                        }
+                    }
+                    Log.i("SUCCESS", "Snapshot success")
+
+                }
+            }
+
     }
 
-
 }
+
+//val messagesCollectionSenderPath =
+//    "users/$currentUserUid/contacts/$currentContactId/messages"
+//val messagesCollectionReceiverPath =
+//    "users/$currentContactId/contacts/$currentUserUid/messages"
+//
+//val collection1Ref =
+//    FirebaseFirestore.getInstance().collection(messagesCollectionSenderPath)
+//val collection2Ref =
+//    FirebaseFirestore.getInstance().collection(messagesCollectionReceiverPath)
+//val combinedChatMessages = ArrayList<ChatMessage>()
+//
+//collection1Ref.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+//    if (firebaseFirestoreException != null) {
+//        println("Lyssnaren på samling 1 orsakade ett fel: $firebaseFirestoreException")
+//        return@addSnapshotListener
+//    }
+//
+//    // Hantera förändringar i samlingen 1 här
+//    querySnapshot?.let { snapshot ->
+//        for (document in snapshot.documents) {
+//            val chatMessage = document.toObject(ChatMessage::class.java)
+//            chatMessage?.let {
+//                combinedChatMessages.add(it)
+//            }
+//
+//        }
+//    }
+//}
+//
+//// Skapa en snapshotlistener för den andra samlingen
+//collection2Ref.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+//    if (firebaseFirestoreException != null) {
+//        println("Lyssnaren på samling 2 orsakade ett fel: $firebaseFirestoreException")
+//        return@addSnapshotListener
+//    }
+//
+//    // Hantera förändringar i samlingen 2 här
+//    querySnapshot?.let { snapshot ->
+//        val chatList = ArrayList<ChatMessage>()
+//        for (document in snapshot.documents) {
+//            val chatMessage = document.toObject(ChatMessage::class.java)
+//            chatMessage?.let {
+//                combinedChatMessages.add(it)
+//            }
+//        }
+//    }
+//}
+
+
